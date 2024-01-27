459a5a4dbe3464be24c9c2f0f62ba61da9ce2fed
HouseMushrooms <djholliday789@gmail.com>
2024-01-27 14:14:59 -0500

    fizzbuzz challenge 1

--- FizzBuzz/Challenge1.java
+++ FizzBuzz/Challenge1.java
@@ -7,8 +7,16 @@ public class Challenge1 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a positive number");
         int numIn = sc.nextInt();
-        if(){
-
+        for (int num = 1; num <= numIn; num++){
+            if(num % 3 == 0){
+            System.out.println("fizz");
+            continue;
+        } else if (num % 5 == 0){
+            System.out.println("buzz");
+            continue;
         }
+            System.out.println(num);
+       }         
+        sc.close();
     }
 }
